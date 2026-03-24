df38c94b960e6d6d573fbeeeeae99e8a4aab6b1d
EvilSquirrelGuy <evil.squirrelo.xtreme@gmail.com>
2026-03-24 17:33:53 +0000

    add maven publish thingy to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
-group = "dev.evilsquirrelguy"
-version = "0.1"
+group = project.property("group") as String
+version = project.property("version") as String
 
 repositories {
     mavenCentral()
@@ -16,6 +17,49 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.test {
-    useJUnitPlatform()
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"]) // add compiled jar
+            groupId = project.property("group") as String
+            artifactId = project.property("artifactId") as String
+            version = project.property("version") as String
+
+            pom {
+                name.set(project.property("name") as String)
+                description.set(project.property("description") as String)
+                url.set(project.property("url") as String)
+
+                licenses {
+                    license {
+                        name.set("Mozilla Public License, version 2.0")
+                        url.set("https://www.mozilla.org/en-US/MPL/2.0/")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("evilsquirrelguy")
+                        name.set("EvilSquirrelGuy")
+                    }
+                }
+            }
+
+
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/EvilSquirrelGuy/JHaaC")
+            credentials {
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
+            }
+        }
+    }
 }
+
+//tasks.test {
+//    useJUnitPlatform()
+//}
